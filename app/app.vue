2adc4c13d93099a5282e1428974c9664d565f4a3
Danil Kolbasenko <danilvusockij@gmail.com>
2026-04-21 22:04:20 +0300

    First commit

---
+++ app/app.vue
@@ -0,0 +1,20 @@
+
+
+<template>
+  <div>
+
+      <NuxtPage />
+      <div>
+        <ComHeader/>
+      </div>
+  </div>
+
+
+
+
+<main>
+    <div>
+        <img src="/Vector 4.svg" alt="">
+    </div>
+</main>
+</template>
---
+++ app/components/ComHeader.vue
@@ -0,0 +1,19 @@
+<template>
+
+<header>
+    <div>
+        <img src="/logo (1).svg" alt="">
+            <p>Episodes</p>
+            <p> About</p>
+            <p>More</p>
+    </div>
+        <div>
+            <button>RECENT EPISODES</button>
+            <button>SUBSCRIBE</button>
+        </div>
+</header>
+
+
+</template>
+
+<script setup lang='ts'></script>
